ac92ea7e77400d2ed46ed5c350e209c1c8f8bbf6
Artyom <toropov.01@bk.ru>
2023-10-14 19:48:11 +0100

    Change function names and fix import

--- seqanalyzer.py
+++ seqanalyzer.py
@@ -1,10 +1,10 @@
 # Importing modules
-import modules.fastq_filter 
-import modules.dna_rna_tools 
-import modules.protein_tools 
+import modules.fastq_filter as ff
+import modules.dna_rna_tools as drt
+import modules.protein_tools as pt
 
 # Function for FASTQ-sequences filtration
-def fastq_filter(input_path: str, 
+def run_fastq_filter(input_path: str, 
                  output_filename: str = None, 
                  gc_bounds = (0, 100), 
                  length_bounds = (0, 2**32), 
@@ -28,22 +28,22 @@ def fastq_filter(input_path: str,
     Note: the output file is saved to the /fastq_filtrator_results 
     directory. The default output file name is the name of the input file.
     '''
-    seqs = read_fastq_file(input_path)
+    seqs = ff.read_fastq_file(input_path)
     filtered_seqs = {}
     for id in seqs:
-        quality_value = check_quality(seqs[id][2], quality_threshold) 
-        length_value = check_length(seqs[id][0], length_bounds)
-        gc_content_value = check_gc_content(seqs[id][0], gc_bounds)
+        quality_value = ff.check_quality(seqs[id][2], quality_threshold) 
+        length_value = ff.check_length(seqs[id][0], length_bounds)
+        gc_content_value = ff.check_gc_content(seqs[id][0], gc_bounds)
         if  quality_value and length_value and gc_content_value == True:
             filtered_seqs[id] = seqs[id]
             
     if output_filename == None:
         output_filename_with_extension = os.path.basename(input_path) 
         output_filename = os.path.splitext(output_filename_with_extension)[0] 
-    write_fastq_file(filtered_seqs, output_filename)
+    ff.write_fastq_file(filtered_seqs, output_filename)
 
 # Function for DNA/RNA sequences analysis
-def dna_rna_tools(*args: str):
+def run_dna_rna_tools(*args: str):
     """
     Function containing methods for DNA or RNA sequences analysis.
 
@@ -61,10 +61,10 @@ def dna_rna_tools(*args: str):
     """
     *seqs, procedure = args
     results = []
-    functions = {'transcribe': transcribe, 
-                 'reverse': reverse,
-                 'reverse_complement': reverse_complement,
-                 'complement': complement}
+    functions = {'transcribe': drt.transcribe, 
+                 'reverse': drt.reverse,
+                 'reverse_complement': drt.reverse_complement,
+                 'complement': drt.complement}
     for seq in seqs:
         if is_rna(seq) and is_dna(seq) is not True:
             raise ValueError("Invalid nucleotide sequence")
@@ -75,7 +75,7 @@ def dna_rna_tools(*args: str):
         return results
 
 # Function for protein sequences analysis
-def protein_tools(*args: str):
+def run_protein_tools(*args: str):
     """
     Function containing methods for protein analysis.
 
@@ -99,13 +99,13 @@ def protein_tools(*args: str):
     *seqs, procedure = args
     results = []
     functions = {
-        "compute_molecular_weight": compute_molecular_weight,
-        "compute_length": compute_length,
-        "compute_hydrophobicity": compute_hydrophobicity,
-        "stats_amino_acids": stats_amino_acids,
-        "get_protein_gene": get_protein_gene
+        "compute_molecular_weight": pt.compute_molecular_weight,
+        "compute_length": pt.compute_length,
+        "compute_hydrophobicity": pt.compute_hydrophobicity,
+        "stats_amino_acids": pt.stats_amino_acids,
+        "get_protein_gene": pt.get_protein_gene
+
     }
-    
     if procedure == "check_mutations":
         results.append(check_mutations(seqs[0], seqs[1]))
     else:
@@ -119,5 +119,4 @@ def protein_tools(*args: str):
     if len(results) == 1:
         return results[0]
     else:
-        return results
-        
+        return results        
