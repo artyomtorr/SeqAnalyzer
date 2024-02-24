fea91e177decb62fa873d29c2eaaddc1cd673d36
Artem <arteem2300@gmail.com>
2024-02-24 14:42:20 +0300

    Implement filter_fastq function and BiologicalSequence class

--- seqanalyzer.py
+++ seqanalyzer.py
@@ -1,11 +1,11 @@
 import os
+from abc import ABC, abstractmethod
+from Bio import SeqIO
+from Bio.SeqUtils import gc_fraction
 from typing import Tuple
-import modules.fastq_filter as ff
-import modules.dna_rna_tools as drt
-import modules.protein_tools as pt
 
 
-def run_fastq_filter(input_path: str, 
+def filter_fastq(input_path: str, 
                  output_filename: str = None, 
                  gc_bounds: Tuple[int, int] = (0, 100), 
                  length_bounds: Tuple[int, int] = (0, 2**32), 
@@ -24,100 +24,116 @@ def run_fastq_filter(input_path: str,
         interval for filtering. Tuple if contains lower and upper 
         bounds, int if only contains an upper bound.
         - quality_threshold (int, default = 0): threshold value of average 
-    read quality for filtering.
+        read quality for filtering.
 
     Note: the output file is saved to the /fastq_filtrator_results 
     directory. The default output file name is the name of the input file.
     '''
-    seqs = ff.read_fastq_file(input_path)
-    filtered_seqs = {}
-    for name in seqs:
-        quality_value = ff.check_quality(seqs[name][2], quality_threshold) 
-        length_value = ff.check_length(seqs[name][0], length_bounds)
-        gc_content_value = ff.check_gc_content(seqs[name][0], gc_bounds)
-        if  quality_value and length_value and gc_content_value:
-            filtered_seqs[name] = seqs[name]
-            
-    if not output_filename:
-        output_filename_with_extension = os.path.basename(input_path) 
-        output_filename = os.path.splitext(output_filename_with_extension)[0] 
-    ff.write_fastq_file(filtered_seqs, output_filename)
+    with open(input_path, "r") as handle:
+        records = [record for record in SeqIO.parse(handle, "fastq")]
+
+    filtered_records = []
+    for record in records:
+        gc_content = gc_fraction(record.seq) * 100
+        seq_length = len(record.seq)
+        avg_quality = sum(record.letter_annotations["phred_quality"]) / seq_length
+
+        if (gc_bounds[0] <= gc_content <= gc_bounds[1] and
+            length_bounds[0] <= seq_length <= length_bounds[1] and
+            avg_quality >= quality_threshold):
+            filtered_records.append(record)
+
+    if not os.path.exists("fastq_filtrator_results"):
+        os.makedirs("fastq_filtrator_results")
+
+    if output_filename is None:
+        output_filename = input_path.split("/")[-1].split(".")[0] + "_filtered.fastq"
+
+    output_path = "fastq_filtrator_results/" + output_filename
+    SeqIO.write(filtered_records, output_path, "fastq")
+
+    print(f"Filtered sequences saved to {output_path}")
 
 
-def run_dna_rna_tools(*args: str):
-    """
-    Function containing methods for DNA or RNA sequences analysis.
+class BiologicalSequence(ABC):
+    def __init__(self, seq: str = None):
+        self.seq = seq
 
-    Takes arbitrary number of arguments with DNA or RNA sequencies
-    and the name of the procedure to be performed (always the last
-    argument). 
-    Returns the result of the procedure as string if one sequence
-    is submitted or list if several.
+    @abstractmethod
+    def __len__(self):
+        pass
 
-    Supported procedures:
-    - 'transcribe' - transcribes DNA sequence to RNA sequence.
-    - 'reverse' - reverses nucleotide sequence.
-    - 'complement' - returns complementary nucleotide sequence.
-    - 'reverse_complement' - returns reversed complementary nucleotide sequence.
-    """
-    *seqs, procedure = args
-    results = []
-    procedures = {'transcribe': drt.transcribe, 
-                 'reverse': drt.reverse,
-                 'reverse_complement': drt.reverse_complement,
-                 'complement': drt.complement}
-    for seq in seqs:
-        if not drt.is_rna(seq) and drt.is_dna(seq):
-            raise ValueError("Invalid nucleotide sequence")
-        results.append(procedures[procedure](seq))
-    if len(results) == 1:
-        return results[0]
-    else: 
-        return results
+    @abstractmethod
+    def __getitem__(self, index):
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    @abstractmethod
+    def check_alphabet(self):
+        pass
 
 
-def run_protein_tools(*args: str):
-    """
-    Function containing methods for protein analysis.
+class NucleicAcidSequence(BiologicalSequence):
+    def __len__(self):
+        return len(self.seq)
 
-    Takes arbitrary number of arguments with protein sequencies
-    and the name of the procedure to be performed (always the last
-    argument). Returns the result of the procedure as string
-    or dictionary if one sequence is submitted or list if several.
+    def __getitem__(self, index):
+        return self.seq[index]
 
-    Note: if procedure 'check_mutations' is used then input must
-    contain only three arguments: RNA sequence, protein sequence
-    and the name of procedure itself.
+    def __repr__(self):
+        return self.seq
 
-    Supported procedures:
-    - 'compute_molecular_weight' — computes molecular weight in g/mol
-    - 'compute_length' — computes the number of amino acids
-    - 'compute_hydrophobicity' — computes the percentage of gydrophobic aminoacids 
-    - 'check_mutations' — checks missense mutations after translation
-    - 'get_protein_gene' - returns possible variants of coding DNA sequences
-    - 'count_amino_acids' - calculates the number of each aminoacid
-    """
-    *seqs, procedure = args
-    results = []
-    procedures = {
-        "compute_molecular_weight": pt.compute_molecular_weight,
-        "compute_length": pt.compute_length,
-        "compute_hydrophobicity": pt.compute_hydrophobicity,
-        "stats_amino_acids": pt.stats_amino_acids,
-        "get_protein_gene": pt.get_protein_gene
+    def check_alphabet(self):
+        return set(self.seq.upper()).issubset(self.ALPHABET)
 
-    }
-    if procedure == "check_mutations":
-        results.append(pt.check_mutations(seqs[0], seqs[1]))
-    else:
-        for seq in seqs:
-            if pt.is_protein(seq) is not True:
-                raise ValueError("Invalid protein sequence")
-            if procedure not in procedures:
-                raise ValueError("Wrong procedure name")
-            else:
-                results.append(procedures[procedure](seq))
-    if len(results) == 1:
-        return results[0]
-    else:
-        return results        
+    def complement(self):
+        comp_seq = self.seq.translate(str.maketrans(self.COMPLEMENT_MAP))
+        return type(self)(comp_seq)
+    
+    def reverse_complement(self):
+        reverse_seq = self.complement().seq[::-1]
+        return type(self)(reverse_seq)
+    
+    def gc_content(self):
+        gc_count = self.seq.count('G') + self.seq.count('C')
+        return gc_count / len(self.seq)
+    
+
+class DNASequence(NucleicAcidSequence):
+    ALPHABET = {'A', 'T', 'G', 'C'}
+    COMPLEMENT_MAP = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
+                      'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
+
+    def transcribe(self):
+        return RNASequence(self.seq.replace('T', 'U'))
+
+
+class RNASequence(NucleicAcidSequence):
+    ALPHABET = {'A', 'G', 'C', 'U'}
+    COMPLEMENT_MAP = {'A': 'U', 'U': 'A', 'G': 'C', 'C': 'G',
+                      'a': 'u', 'u': 'a', 'g': 'c', 'c': 'g'}
+
+
+class AminoAcidSequence(BiologicalSequence):
+    ALPHABET = {"A", "C", "D", "E", "F", "G", "H", "I","K", "L", 
+                "M", "N","P", "Q", "R", "S", "T", "V", "W", "Y"}
+    HYDROPHOBIC_AMINOACIDS = {"A", "V", "L", "I", "M", "F", "Y", "W"}
+    
+    def __len__(self):
+        return len(self.seq)
+
+    def __getitem__(self, index):
+        return self.seq[index]
+
+    def __repr__(self):
+        return self.seq
+
+    def check_alphabet(self):
+        return set(self.seq.upper()).issubset(self.ALPHABET)
+    
+    def compute_hydrophobicity(self):
+        hydrophobic_count = sum(1 for aa in self.seq if aa.upper() in self.HYDROPHOBIC_AMINOACIDS)
+        return (hydrophobic_count / len(self.seq)) * 100
